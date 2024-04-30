4f145d02bac3fc7a0bb0fff9a65fd2ef07748b6e
Utkarsh Vardhan <vardhanutkarsh25@gmail.com>
2024-04-30 05:43:59 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// program 1
+class HelloWorld
+{
+
+public static void main(String[] args)
+{
+	int a; // declaration
+	a=10; // define
+	//int a=10; 
+System.out.println("The value of a="+a);
+}
+}
---
+++ Program3.java
@@ -0,0 +1,13 @@
+// program 2
+class Program3
+
+{
+
+public static void main(String[] args)
+{
+	int a; // declaration // local variable should be initialized
+	a=10; // define
+	//int a=10; 
+System.out.println("The value of a="+a);
+}
+}
